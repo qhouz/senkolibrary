c1b7b87a1cc83951f40150c132f4dde4eb9e42be
qhouz <73645601+qhouz@users.noreply.github.com>
2022-08-17 21:02:04 +0700

    Update prepackage.lua

--- gamesense/senkotech/prepackage.lua
+++ gamesense/senkotech/prepackage.lua
@@ -1,6 +1,6 @@
 -- originals
 local table_insert = table.insert;
-local client_set_event_callback, client_delay_call = client.set_event_callback, client.delay_call;
+local client_color_log, client_set_event_callback, client_delay_call = client.color_log, client.set_event_callback, client.delay_call;
 local ui_set_callback, ui_set_visible, ui_update, ui_get, ui_set = ui.set_callback, ui.set_visible, ui.update, ui.get, ui.set;
 
 -- defines
